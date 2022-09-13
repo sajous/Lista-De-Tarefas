9c5072c0deda12211e9cf79643020814f46545d0
Patrick Sajous <patricksajous17@gmail.com>
2022-09-13 03:09:11 -0300

    feat: acabar com sublinhado

--- lista-tarefas/src/pages/Home.tsx
+++ lista-tarefas/src/pages/Home.tsx
@@ -1,13 +1,18 @@
-import { Button, Checkbox, TextField } from "@mui/material";
-import React, { ReactEventHandler, useCallback, useState } from "react"
+import { Button, Checkbox } from "@mui/material";
+import React from "react";
+import { useState, useEffect} from "react"
 import { FaTrash , FaEdit} from 'react-icons/fa'
 import './home.css'
 
-export const Home = () => {
 
-    
+
+export const Home = (props :any) => {
+
+
     const [item, setItem]:any = useState('');
     const [itemList, setItemList]:any = useState([])
+    const [check , setCheck] :any = useState()
+    const [letra , setLetra] :any = useState()
 
     const addItem = () => {
         if( item.length === 0) return;
@@ -23,18 +28,25 @@ export const Home = () => {
         }
       }
 
+      const teste = (props: any) => { 
+        setLetra((e:any) => check ? "'risca'" : "text-decoration: underline wavy red;");
+        var teste1 = letra
+        props.teste = teste1
+        console.log(props.teste)
+      };
 
-    return (
+    return(
         <div className="container">
             <h1>Minhas Tarefas</h1>
       <input type="text" placeholder="Item" value={item} name="item" onChange = {e => setItem(e.target.value)} onKeyDown={handleKeyPress} />
         <Button variant="text" className="Button" onClick={addItem}>➕</Button>
       <ul>
         {itemList.map((item:any) => (
-          <li><Checkbox inputProps={{ 'aria-label': 'controlled' }} />{item}<a href=""><FaTrash /></a> <a href=""><FaEdit /></a></li>
+          <li><Checkbox checked={check} onChange={teste} inputProps={{ 'aria-label': 'controlled' }} /><div style={props.teste}>{item}</div><a href=""><FaTrash /></a> <a href=""><FaEdit /></a></li>
         ))}
       </ul>
          </div>
     )
         
 }
+
